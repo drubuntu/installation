9344caa7d458583a30867b55a89a98b53b83f50a
=Horvan <=drubuntu@outlook.com>
2015-01-22 23:13:16 +0100

    strings changed

--- en.sh
+++ en.sh
@@ -1,30 +1,46 @@
+#!/bin/bash
+# welcom messages
 heightfin="-"
-wcmssg1=Welcome to Drubuntu - Setup part 2
-wcmssg2=This assistent would do everything for you. 
-wcmssg3=This is just the basic installation later you'll be able to add more features and set up your the desktop of you like.
-aktcmssg=Hit any key to continue!
-bppmssg=Be pacient please ...
-dlbcmssg=Drubuntu downloads all basic components now
-ap2setupmssg=Preparing Appache 2 for Drupal
-d7installmssg=Installing Drupal 7
-d8installmssg=Installing Drupal 8
-drushinstallmssg=Installing Drush
-createdirmsg=Creating folders ...
-createfilesmsg=Creating files ...
-createmysqldbmssg=Create mysql Database ---
-prpusrflsmssg=Preparing user files ...
-createusermssg=create user
-disablerootmssg=disabeling root account
-plymoutlogomessage=changeig Plymouth Boot Logo
-roothintmssg=You are logged in as root. Answer the questions and log in with your specified user name!
-enterusernamemmsg=Enter the username you like to use later and hit [ENTER]: 
-$wwoddltumssg=Wich Version of Drupal do you like to use? 
-drupal7inputdatamssg=Now we need some Data for Drupal7:
-enterd7domainmssg=Enter the domain for Drupal 7 and press [ENTER]: 
-enterd7dbusermssg=Enter the name of Your Drupal 7 database user and press [ENTER]: 
-enterd7dbpassmssg=Enter the password for Your Drupal 7 Database user and press [ENTER]: 
-$drupal7inputdatamssg=Now we need some Data for Drupal8:
-enterd8domainmssg=Enter the domain for drupal 8 and press [ENTER]: 
-enterd8dbusermssg=Enter the name of Your Drupal 8 database user and press [ENTER]: 
-enterd8dbpassmssg=Enter the password for Your Drupal 8 Database user and press [ENTER]: 
-thanksmssg=Thank You!
+wcmssg1="Welcom to the second part of Drubuntus setup"
+wcmssg2="this assistent will do the rest for you" 
+wcmssg3="This is just the base system, You can add extra features later."
+#standard messages
+aktcmssg="press any key to contineu"
+bppmssg="Just a moment, please ..."
+thanksmssg="Thank you!"
+
+#messages acording to the file system
+dlbcmssg="Drubuntu is installing all basic components, now"
+createdirmsg="Creating directories ..."
+createfilesmsg="Creating files ..."
+
+#LAMP
+ap2setupmssg="Apache 2 is preparing for Drupal..."
+createmysqldbmssg="Creating MySql Databases ---"
+
+#Drupal
+d7installmssg="installing Drupal 7  ..."
+d8installmssg="installing Drupal 8  ..."
+drushinstallmssg="installing Drush  ..."
+
+
+plymoutlogomessage="installng plymouth boot logo"
+
+wvtumssg="Witch version of drupal do you like to use"
+
+drupal7inputdatamssg="Nun benötigen wir einige Angaben für Drupal 7:"
+
+enterd7domainmssg="Geben Sie die Domain für Drupal 7 ein und drücken Sie [ENTER]: "
+
+enterd7dbusermssg="Geben Sie den Namen des Datenbankbenutzers für Drupal 7 an und drücken Sie [ENTER]: "
+
+enterd7dbpassmssg="Geben ise das Passwort für Ihren Drupal 7 Datenbankbenutzer an und drücken Sie [ENTER]: "
+
+drupal7inputdatamssg="Nun benötigen wir einige Daten für Drupal7:"
+
+enterd8domainmssg="Geben Sie die Domain für Drupal 8 an und drüecken Sie [ENTER]: "
+
+enterd8dbusermssg="Geben Sie den Namen Ihres Drupal 8 Datenbanbenutzers an und drücken Sie [ENTER]: "
+
+enterd8dbpassmssg="Geben Sie das Passwort für den Datenbaknutzer von Drupal 8 an und drücken Sie [ENTER]: "
+
