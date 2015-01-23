1d55e00550e3e85952c1d3879c8b73d01a1c958e
Horvan <drubuntu@outlook.com>
2015-01-23 17:50:41 +0100

    Update features.sh

--- features.sh
+++ features.sh
@@ -19,12 +19,11 @@ if [ `whoami` != root ]; then
     4
 fi
 fdir="/opt/.drubuntu/features"
-
+DLURL=https://raw.githubusercontent.com/drubuntu/features/master
 if  [  -d "$fdir" ];then
 cd "$fdir"
 else
-git clone https://github.com/drubutu/features.git "$fdir" 
-cd "$fdir" 
+mkdir -p "fdir" 
 fi
 function show_help() {							#this is the help text
      
@@ -50,26 +49,31 @@ case $1 in
 
     --apc)
 echo -e " ${white}  ${needpassmssg} ${shortapcmssg} ${toyoursystemmssg} ${NC}"
+wget -O "$DIRURL"apc.sh "$DLUURL"/apc.sh
 sudo bash apc.sh
 
     ;;
 
     --aptana)								#the following commands download features install them and remove the files before finishing
 echo -e " ${white}  ${needpassmssg} ${shortaptanaidemssg} ${toyoursystemmssg} ${NC}"
+wget -O "$DIRURL"aptana.ide "$DLUURL"/aptanade.sh
 sudo bash aptanaide.sh
     ;;
 --aptana-git)
 echo -e " ${white}  ${needpassmssg} ${shortaptanagitmssg} ${toyoursystemmssg} ${NC}"
+wget -O "$DIRURL"aptanagitsupport.sh "$DLUURL"/aptanagitsupport.sh
 sudo bash aptanagitsupport.sh
     ;;
 
 --aptana-xdebug)
 echo -e " ${white}  ${needpassmssg} ${shortaptanaxdvugmssg} ${toyoursystemmssg} ${NC}"
+wget -O "$DIRURL"xdebug-aptana "$DLUURL"/xdbug-aptana.sh
 sudo bash xdebug-aptana.sh
     ;;
 
 --atom)
 echo -e " ${white}  ${needpassmssg} ${shortatommssg} ${toyoursystemmssg} ${NC}"
+wget -O "$DIRURL"atom.sh "$DLUURL"/atom.sh
 sudo bash atom.sh >>/dev/null 2>&1
     ;;
 
@@ -77,36 +81,44 @@ sudo bash atom.sh >>/dev/null 2>&1
 
 --memcached)
 echo -e " ${white}  ${needpassmssg} ${shortmemcachedmssg} ${toyoursystemmssg} ${NC}"
+wget -O "$DIRURL"memcached.sh "$DLUURL"/memcached.sh
 sudo bash memcached.sh
     ;;
 
 --proftpd)
 echo -e " ${white}  ${needpassmssg} ${shortüroftpdmssg} ${toyoursystemmssg} ${NC}"
+wget -O "$DIRURL"proftpd.sh "$DLUURL"/proftpd.sh
 sudo  bash proftpd.sh
     ;;
     --xdebug)
 echo -e " ${white}  ${needpassmssg} ${shortxdebugmssg} ${toyoursystemmssg} ${NC}"
+wget -O "$DIRURL"xdebug.sh "$DLUURL"/xdebug.sh
 sudo bash xdebug.sh
     ;;
 --plymouth)
 echo -e " ${white}  ${needpassmssg} ${shortplymouthmssg} ${toyoursystemmssg} ${NC}"
+wget -O "$DIRURL"plymouthlogo.sh "$DLUURL"/plymouthlogo.sh
 sudo bash plymouthlogo.sh
     ;;
     --webexplangs)
 echo -e " ${white}  ${needpassmssg} ${shortwebexplangsmssg} ${toyoursystemmssg} ${NC}"
+wget -O "$DIRURL"webexplangs.sh "$DLUURL"/webexplangs.sh
 sudo bash webexplangs.sh
     ;;
     --varnish)
 echo -e " ${white}  ${needpassmssg} ${shortvarnishmssg} ${toyoursystemmssg} ${NC}"
+wget -O "$DIRURL"varnish.sh "$DLUURL"/varnish.sh
 sudo bash varnish.sh
     ;;
 --browsers)
 echo -e " ${white}  ${needpassmssg} ${shortbrowsersmssg} ${toyoursystemmssg} ${NC}"
+wget -O "$DIRURL"browsersuite.sh "$DLUURL"/browsersuite.sh
 sudo bash browsersuite.sh
 ;;
 
 --bootstrap)
 echo -e " ${white}  ${needpassmssg} ${shortshortbootstrapwsassmssgmssg} ${toyoursystemmssg} ${NC}"
+wget -O "$DIRURL"bootstrapwithsass.sh "$DLUURL"/bootstrapwithsass.sh
 sudo bash bootstrapwithsass.sh
 ;;
 
