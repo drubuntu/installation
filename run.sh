cd4fd67ef5d6505d1696b25a4653dcf7ff8200f3
Horvan <drubuntu@outlook.com>
2015-01-23 13:48:59 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 
 #!/bin/bash
+if [ "$LANG" == "de_DE.UTF-8" ];then
+echo "Um Drubuntu zu installieren geben Sie Ihr Passwort ein oder drücken Sie STRG+C um abzubrechen."
+else
+echo "You'll install Drubuntu now! Enter your passwort  or hit CTRL+C to abbort installation"
+fi
 echo "You'll install Drubuntu now! Enter your passwort  or hit CTRL+C to abbort installation"
 sudo apt -y install git
 git clone https://github.com/drubuntu/installation $HOME/installation
