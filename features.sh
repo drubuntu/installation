7e10d34bd9ad904e1d7ff51d4389ecb8cec34d3a
Horvan <drubuntu@outlook.com>
2015-01-23 16:26:22 +0100

    translation updated
    translation strings updated

--- features.sh
+++ features.sh
@@ -8,20 +8,20 @@ source $PWD/en.sh
 fi
 function show_help() {							#this is the help text
      
-       echo -e " ${white} ${apcswitch} ${doublehighfin}   ${apcmssg} ${NC}"
-       echo -e " ${white}  ${aptanaideswitch} ${aptanaidemssg} ${NC}"
-       echo -e " ${white}  ${aptanagitswitch} ${aptanagitmssg} ${NC}"
-       echo -e " ${white}  ${aptanaxdebugswitch} ${aptanaxdebugmssg} ${NC}"
-       echo -e " ${white}  ${atomswitch} ${atommssg} ${NC}"
-       echo -e " ${white}  ${bootstrapwithsassswitch} ${bootstrapwsassmssg} ${NC}"
-       echo -e " ${white}  ${browsersswitch} ${browsersmssg} ${NC}"
-       echo -e " ${white}  ${desktopswitch} ${desktopsmssg} ${NC}"
-       echo -e " ${white}  ${memcachedswitch} ${memcachedmssg} ${NC}"
-       echo -e " ${white}  ${plymouthswitch} ${plymouthmssg} ${NC}"
-       echo -e " ${white}  ${proftpdswitch}${proftpdmssg} ${NC}"  .
-       echo -e " ${white}  ${varnishshwitch} ${varnishmssg} ${NC}"
-       echo -e " ${white}  ${webexplangsswitch} ${webexplangsmssg} ${NC}"
-       echo -e " ${white}  ${xdebugswitch} ${xdebugmssg} ${NC}"
+       echo -e " ${white}  ${apcswitch}                ${apcmssg} ${NC}"
+       echo -e " ${white}  ${aptanaideswitch}          ${aptanaidemssg} ${NC}"
+       echo -e " ${white}  ${aptanagitswitch}          ${aptanagitmssg} ${NC}"
+       echo -e " ${white}  ${aptanaxdebugswitch}       ${aptanaxdebugmssg} ${NC}"
+       echo -e " ${white}  ${atomswitch}               ${atommssg} ${NC}"
+       echo -e " ${white}  ${bootstrapwithsassswitch}  ${bootstrapwsassmssg} ${NC}"
+       echo -e " ${white}  ${browsersswitch}           ${browsersmssg} ${NC}"
+       echo -e " ${white}  ${desktopswitch}            ${desktopsmssg} ${NC}"
+       echo -e " ${white}  ${memcachedswitch}          ${memcachedmssg} ${NC}"
+       echo -e " ${white}  ${plymouthswitch}           ${plymouthmssg} ${NC}"
+       echo -e " ${white}  ${proftpdswitch}            ${proftpdmssg} ${NC}"  .
+       echo -e " ${white}  ${varnishshwitch}           ${varnishmssg} ${NC}"
+       echo -e " ${white}  ${webexplangsswitch}        ${webexplangsmssg} ${NC}"
+       echo -e " ${white}  ${xdebugswitch}             ${xdebugmssg} ${NC}"
 
 }
 
@@ -39,7 +39,7 @@ fdir="/opt/.drubuntu/features"
 if  [  -d "$fdir" ];then
 cd "$fdir"
 else
-git clone "$repofeatures" "$bdir/features" >> /dev/null 2>&1
+git clone "$repofeatures" "$fdir" >> /dev/null 2>&1
 cd "$fdir" 
 fi
 
@@ -49,64 +49,64 @@ case $1 in
     ;;
 
     --apc)
-echo "We need your password to install apc cache on your system"
+echo -e " ${white}  ${needpassmssg} ${shortapcmssg} ${toyoursystemmssg} ${NC}"
 sudo bash apc.sh
 
     ;;
 
     --aptana)								#the following commands download features install them and remove the files before finishing
-echo "We need your password to finish Setup of Aptana Studio:"
+echo -e " ${white}  ${needpassmssg} ${shortaptanaidemssg} ${toyoursystemmssg} ${NC}"
 sudo bash aptanaide.sh
     ;;
 --aptana-git)
-echo "We need your password to integrate Git functionality:"
+echo -e " ${white}  ${needpassmssg} ${shortaptanagitmssg} ${toyoursystemmssg} ${NC}"
 sudo bash aptanagitsupport.sh
     ;;
 
 --aptana-xdebug)
-echo "We need your password to integrate Git functionality:"
+echo -e " ${white}  ${needpassmssg} ${shortaptanaxdvugmssg} ${toyoursystemmssg} ${NC}"
 sudo bash xdebug-aptana.sh
     ;;
 
 --atom)
-echo "We need your password to integrate Git functionality:"
+echo -e " ${white}  ${needpassmssg} ${shortatommssg} ${toyoursystemmssg} ${NC}"
 sudo bash atom.sh >>/dev/null 2>&1
     ;;
 
 
 
 --memcached)
-echo "We need your password to install memcached to your system:"
+echo -e " ${white}  ${needpassmssg} ${shortmemcachedmssg} ${toyoursystemmssg} ${NC}"
 sudo bash memcached.sh
     ;;
 
 --proftpd)
-echo "To install ProFTPd enter your password, please."
+echo -e " ${white}  ${needpassmssg} ${shortüroftpdmssg} ${toyoursystemmssg} ${NC}"
 sudo  bash proftpd.sh
     ;;
     --xdebug)
-echo " To install XDebug enter your password, please."
+echo -e " ${white}  ${needpassmssg} ${shortxdebugmssg} ${toyoursystemmssg} ${NC}"
 sudo bash xdebug.sh
     ;;
 --plymouth)
-echo " To install the plymouth theme for drubuntu enter your password, please."
+echo -e " ${white}  ${needpassmssg} ${shortplymouthmssg} ${toyoursystemmssg} ${NC}"
 sudo bash plymouthlogo.sh
     ;;
     --webexplangs)
-echo "To install the ruby based expression language and processors enter your password, please."
+echo -e " ${white}  ${needpassmssg} ${shortwebexplangsmssg} ${toyoursystemmssg} ${NC}"
 sudo bash webexplangs.sh
     ;;
     --varnish)
-echo "To install Varnish on your system enter your password, please."
+echo -e " ${white}  ${needpassmssg} ${shortvarnishmssg} ${toyoursystemmssg} ${NC}"
 sudo bash varnish.sh
     ;;
 --browsers)
-echo "To install the Browser Suite on your system enter your password, please"
+echo -e " ${white}  ${needpassmssg} ${shortbrowsersmssg} ${toyoursystemmssg} ${NC}"
 sudo bash browsersuite.sh
 ;;
 
 --bootstrap)
-echo "To install the Bootstrap 3 boilerplate with sass on system enter your password, please"
+echo -e " ${white}  ${needpassmssg} ${shortshortbootstrapwsassmssgmssg} ${toyoursystemmssg} ${NC}"
 sudo bash bootstrapwithsass.sh
 ;;
 
