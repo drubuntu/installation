3aea2e17d1d28448ec2c2c0b732ec448ba2b4cba
Horvan <drubuntu@outlook.com>
2015-01-23 16:31:20 +0100

    Update setupsite-d8.sh

--- setupsite-d8.sh
+++ setupsite-d8.sh
@@ -1,3 +1,4 @@
+cd ${0%/*}
 drush -y si standard --db-url=mysql://drupal:drupal@localhost/drupal8 --db-su=root --db-su-pw=MyRoot --account-pass=123456 --site-name="Drupal" >>/dev/null 1>&2;
 drush -y dl smtp-8.x-1.x-dev >>/dev/null 1>&2
 exit
