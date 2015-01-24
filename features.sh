b1af9d986ff3bc8e6afad1de44ee41c39e4ed6b8
Horvan <drubuntu@outlook.com>
2015-01-24 02:49:26 +0100

    Update features.sh

--- features.sh
+++ features.sh
@@ -35,38 +35,29 @@ shelled=shelled.sh
 varnish=varnish.sh
 webexplangs=webexplangs.sh
 xdbug=xdbug.sh
+function download(){
+curl -Ls -A "$AGENT" -O $apc $DLURL
+curl -Ls -A "$AGENT" -O $aptanagitsupport $DLURL
+curl -Ls -A "$AGENT" -O $aptanaide $DLURL
+curl -Ls -A "$AGENT" -O $atom $DLURL
+curl -Ls -A "$AGENT" -O $bootstrapwithsass $DLURL
+curl -Ls -A "$AGENT" -O $memcached $DLURL
+curl -Ls -A "$AGENT" -O $plymouthlogo $DLURL
+curl -Ls -A "$AGENT" -O $proftpd $DLURL
+curl -Ls -A "$AGENT" -O $shelled $DLURL
+curl -Ls -A "$AGENT" -O $varnish $DLURL
+curl -Ls -A "$AGENT" -O $webexplangs $DLURL
+curl -Ls -A "$AGENT" -O $aptanaxdbug $DLURL
+curl -Ls -A "$AGENT" -O $xdbug $DLURL
+}
+
 if ! [  -d "$DIRURL" ];then
 mkdir -p "$DIRURL" 
-curl -Ls -A "$AGENT" -O $DLURL$apc
-curl -Ls -A "$AGENT" -O $DLURL$aptanagitsupport
-curl -Ls -A "$AGENT" -O $DLURL$aptanaide
-curl -Ls -A "$AGENT" -O $DLURL$atom
-curl -Ls -A "$AGENT" -O $DLURL$bootstrapwithsass
-curl -Ls -A "$AGENT" -O $DLURL$memcached
-curl -Ls -A "$AGENT" -O $DLURL$plymouthlogo
-curl -Ls -A "$AGENT" -O $DLURL$proftpd
-curl -Ls -A "$AGENT" -O $DLURL$shelled
-curl -Ls -A "$AGENT" -O $DLURL$varnish
-curl -Ls -A "$AGENT" -O $DLURL$webexplangs
-curl -Ls -A "$AGENT" -O $DLURL$aptanaxdbug
-curl -Ls -A "$AGENT" -O $DLURL$xdbug
 else
-"$DIRURL" 
-rm "$DIRURL"*.sh
-curl -Ls -A "$AGENT" -O $DLURL$apc
-curl -Ls -A "$AGENT" -O $DLURL$aptanagitsupport
-curl -Ls -A "$AGENT" -O $DLURL$aptanaide
-curl -Ls -A "$AGENT" -O $DLURL$atom
-curl -Ls -A "$AGENT" -O $DLURL$bootstrapwithsass
-curl -Ls -A "$AGENT" -O $DLURL$memcached
-curl -Ls -A "$AGENT" -O $DLURL$plymouthlogo
-curl -Ls -A "$AGENT" -O $DLURL$proftpd
-curl -Ls -A "$AGENT" -O $DLURL$shelled
-curl -Ls -A "$AGENT" -O $DLURL$varnish
-curl -Ls -A "$AGENT" -O $DLURL$webexplangs
-curl -Ls -A "$AGENT" -O $DLURL$aptanaxdbug
-curl -Ls -A "$AGENT" -O $DLURL$xdbug
+rm -r "$DIRURL"
+mkdir -p "$DIRURL"
 fi
+download
 function show_help() {							#this is the help text
      
        echo -e " ${white}  ${apcswitch}                ${apcmssg} ${NC}"
@@ -91,31 +82,27 @@ case $1 in
 
     --apc)
 echo -e " ${white}  ${needpassmssg} ${shortapcmssg} ${toyoursystemmssg} ${NC}"
-wget -O "$DIRURL"apc.sh "$DLUURL"/apc.sh
+
 sudo bash apc.sh
 
     ;;
 
     --aptana)								#the following commands download features install them and remove the files before finishing
 echo -e " ${white}  ${needpassmssg} ${shortaptanaidemssg} ${toyoursystemmssg} ${NC}"
-wget -O "$DIRURL"aptana.ide "$DLUURL"/aptanade.sh
 sudo bash aptanaide.sh
     ;;
 --aptana-git)
 echo -e " ${white}  ${needpassmssg} ${shortaptanagitmssg} ${toyoursystemmssg} ${NC}"
-wget -O "$DIRURL"aptanagitsupport.sh "$DLUURL"/aptanagitsupport.sh
 sudo bash aptanagitsupport.sh
     ;;
 
 --aptanaxdebug)
 echo -e " ${white}  ${needpassmssg} ${shortaptanaxdvugmssg} ${toyoursystemmssg} ${NC}"
-wget -O "$DIRURL"xdebug-aptana "$DLUURL"/xdbug-aptana.sh
 sudo bash aptanaxdbug.sh
     ;;
 
 --atom)
 echo -e " ${white}  ${needpassmssg} ${shortatommssg} ${toyoursystemmssg} ${NC}"
-wget -O "$DIRURL"atom.sh "$DLUURL"/atom.sh
 sudo bash atom.sh >>/dev/null 2>&1
     ;;
 
@@ -123,44 +110,36 @@ sudo bash atom.sh >>/dev/null 2>&1
 
 --memcached)
 echo -e " ${white}  ${needpassmssg} ${shortmemcachedmssg} ${toyoursystemmssg} ${NC}"
-wget -O "$DIRURL"memcached.sh "$DLUURL"/memcached.sh
 sudo bash memcached.sh
     ;;
 
 --proftpd)
 echo -e " ${white}  ${needpassmssg} ${shortüroftpdmssg} ${toyoursystemmssg} ${NC}"
-wget -O "$DIRURL"proftpd.sh "$DLUURL"/proftpd.sh
 sudo  bash proftpd.sh
     ;;
     --xdebug)
 echo -e " ${white}  ${needpassmssg} ${shortxdebugmssg} ${toyoursystemmssg} ${NC}"
-wget -O "$DIRURL"xdebug.sh "$DLUURL"/xdebug.sh
 sudo bash xdebug.sh
     ;;
 --plymouth)
 echo -e " ${white}  ${needpassmssg} ${shortplymouthmssg} ${toyoursystemmssg} ${NC}"
-wget -O "$DIRURL"plymouthlogo.sh "$DLUURL"/plymouthlogo.sh
 sudo bash plymouthlogo.sh
     ;;
     --webexplangs)
 echo -e " ${white}  ${needpassmssg} ${shortwebexplangsmssg} ${toyoursystemmssg} ${NC}"
-wget -O "$DIRURL"webexplangs.sh "$DLUURL"/webexplangs.sh
 sudo bash webexplangs.sh
     ;;
     --varnish)
 echo -e " ${white}  ${needpassmssg} ${shortvarnishmssg} ${toyoursystemmssg} ${NC}"
-wget -O "$DIRURL"varnish.sh "$DLUURL"/varnish.sh
 sudo bash varnish.sh
     ;;
 --browsers)
 echo -e " ${white}  ${needpassmssg} ${shortbrowsersmssg} ${toyoursystemmssg} ${NC}"
-wget -O "$DIRURL"browsersuite.sh "$DLUURL"/browsersuite.sh
 sudo bash browsersuite.sh
 ;;
 
 --bootstrap)
 echo -e " ${white}  ${needpassmssg} ${shortshortbootstrapwsassmssgmssg} ${toyoursystemmssg} ${NC}"
-wget -O "$DIRURL"bootstrapwithsass.sh "$DLUURL"/bootstrapwithsass.sh
 sudo bash bootstrapwithsass.sh
 ;;
 
