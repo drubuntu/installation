5c5d4ea8559d9eb47b8b8e87bb6c3f89a7e22b4c
Horvan <drubuntu@outlook.com>
2015-01-24 16:56:21 +0100

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -25,7 +25,7 @@ featuresrepofolder=$HOME/features/
 desktopsrepofolder=$HOME/desktops/
 bdir=/opt/.drubuntu
 #files and folders affected by installation
-query=dpkg-query -W -f '${Status}\n'
+query=`dpkg-query -W -f '${Status}\n'`
 ok="install ok installed"
 ap2_cffile_d7=drupal7.conf
 ap2_cffile_d8=drupal8.conf
@@ -108,62 +108,62 @@ if ! [ `$query` software-properties-common == "$ok" ]  ;then
 apt -qq -y install  software-properties-common >> /dev/null 2>&1
 fi
 
-if ! [ `$query`  curl == "$ok" ] ;then 
+if ! [ $query  curl == "$ok" ] ;then 
 apt -y -qq install curl >> /dev/null 2>&1
 fi
-if ! [ `$query` php-pear == "$ok" ] ;then
+if ! [ $query php-pear == "$ok" ] ;then
 apt install -qq -y php-pear >> /dev/null 2>&1
 fi
-if ! [ `$query` php5-dev == "$ok" ] ;then
+if ! [ $query php5-dev == "$ok" ] ;then
 apt install -qq -y php5-dev >> /dev/null 2>&1
 fi
-if ! [ `$query` php5-curl == "$ok" ] ;then
+if ! [ $query php5-curl == "$ok" ] ;then
 apt install -qq -y php5-curl >> /dev/null 2>&1
 fi
-if ! [ `$query` php5-json == "$ok" ] ;then
+if ! [ $query php5-json == "$ok" ] ;then
 apt install -qq -y php5-json >> /dev/null 2>&1
 fi
-if ! [ `$query` php5-gd == "$ok" ] ;then  
+if ! [ $query php5-gd == "$ok" ] ;then  
 apt install -qq -y php5-gd >> /dev/null 2>&1
 fi
-if ! [ `$query` git == "$ok" ] ;then 
+if ! [ $query git == "$ok" ] ;then 
 apt install -qq -y git >> /dev/null 2>&1
 fi
-if ! [ `$query` git-core == "$ok" ] ;then
+if ! [ $query git-core == "$ok" ] ;then
 apt install -qq -y git-core >> /dev/null 2>&1
 fi
-if ! [ `$query` ruby1.9.1-full == "$ok" ] ;then
+if ! [ $query ruby1.9.1-full == "$ok" ] ;then
 apt install -qq -y ruby1.9.1-full >> /dev/null 2>&1
 fi
-if ! [ `$query` libapache2-mod-php5 == "$ok" ] ;then
+if ! [ $query libapache2-mod-php5 == "$ok" ] ;then
 apt install -qq -y -qq libapache2-mod-php5 >> /dev/null 2>&1 
 fi
-if ! [ `$query` php5-mcrypt == "$ok" ] ; then
+if ! [ $query php5-mcrypt == "$ok" ] ; then
 apt install -qq -y -qq php5-mcrypt >> /dev/null 2>&1
 fi
 
-if ! [ `$query` php5-gd == "$ok" ] ; then
+if ! [ $query php5-gd == "$ok" ] ; then
 apt install -qq -y -qq  php5-gd >> /dev/null 2>&1
 fi
 
-if ! [ `$query` php5-dev == "$ok" ]  ; then
+if ! [ $query php5-dev == "$ok" ]  ; then
 apt install -qq -y -qq  php5-dev >> /dev/null 2>&1
 fi
 
-if ! [ `$query` make == "$ok" ] ; then
+if ! [ $query make == "$ok" ] ; then
 apt install -qq -y make >> /dev/null 2>&1
 fi
 
-if ! [ `$query` build-essential == "$ok" ] ;  then
+if ! [ $query build-essential == "$ok" ] ;  then
 apt -qq -y install  build-essential >> /dev/null 2>&1
 fi
-if ! [ `$query` nodejs == "$ok" ] ;then
+if ! [ $query nodejs == "$ok" ] ;then
 updatenodejs >> /dev/null 2>&1
 fi
-if ! [ `$query` nodejs-legacy == "$ok" ] ; then
+if ! [ $query nodejs-legacy == "$ok" ] ; then
 apt install -qq -y nodejs-legacy >> /dev/null 2>&1 
 fi
-if ! [ `$query` npm == "$ok" ] ;then
+if ! [ $query npm == "$ok" ] ;then
 apt install -qq -y  npm >> /dev/null 2>&1
 fi
 
