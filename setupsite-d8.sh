6bd8c0bac433c4260c0ee905e0468b43c6f09b8a
Horvan <drubuntu@outlook.com>
2016-04-03 05:47:32 +0200

    Update setupsite-d8.sh

--- setupsite-d8.sh
+++ setupsite-d8.sh
@@ -1,4 +1,34 @@
+setfile=/var/www/d8/sites/default/settings.php
+defsetfile=/var/www/d8/sites/default/default.settings.php
+
 cd ${0%/*}
+
+ if [[ $EUID -ne 0 ]]; then
+ echo "Please run as root"
+sudo $0
+  else
+source $HOME/.bashrc
+if [[ -f "$setfile" ]];then
+sudo rm "$setfile"
+sudo cp -R "$defsetfile" "$setfile"
+sudo cat <<EOF >> sites/default/settings.php
+
+
+\$settings['trusted_host_patterns'] = array(
+   '^drupal8\.dev$',
+   '^.+\.drupal8\.dev$',
+   '^.+\.drupal8\.dev',
+   '^localhost$',
+   '^localhost\.*',
+   '\.local$',
+);
+EOF
+fi
+
 drush -y si standard --db-url=mysql://drupal:drupal@localhost/drupal8 --db-su=root --db-su-pw=MyRoot --account-pass=123456 --site-name="Drupal" >>/dev/null 1>&2;
 drush -y dl smtp-8.x-1.x-dev >>/dev/null 1>&2
+sudo chmod 644 sites/default/services.yml
+sudo chmod -R 777 sites/default/files
+sudo chmod 644 sites/default/settings.php
+fi
 exit
