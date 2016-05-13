704bc8a2438ed0b4788b5e5c088f862b96d3af23
Horvan <drubuntu@outlook.com>
2016-05-13 10:41:01 +0200

    added drush

--- setupsite-d7.sh
+++ setupsite-d7.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd ${0%/*}
+drush --version
+git config drush.version 7
+drush --version
 drush -y si standard --db-url=mysql://drupal:drupal@localhost/drupal7 --db-su=root --db-su-pw=MyRoot --account-pass=123456 --site-name="Drupal" >>/dev/null 1>&2;
 drush -y dl l10n_update drush_language smtp >>/dev/null 1>&2
 exit
