eb8c15f95e8acd2359ef3f250da565081c957197
Horvan <drubuntu@outlook.com>
2016-05-15 13:09:37 +0200

    dev/nulld elementarys installatoion

--- functions.sh
+++ functions.sh
@@ -409,17 +409,16 @@ ln -s /etc/issue.net /etc/issue
 }
 
 elementaryos(){
-apt -y install software-properties-common
-apt -y install xorg lightdm
-apt-add-repository -y ppa:gnome3 &&
-apt-add-repository -y ppa:gnome3-team/gnome3-staging &&
-apt-add-repository -y ppa:elementary/stable &&
-apt-add-repository -y ppa:elementary/os patches &&
-apt update &&
-apt-get -y --force-yes -qq install elementary-desktop
-apt-get -y remove unity-greeter &&
-dpg-reconfigure pantheon-greeter &&
-apt-get upgrade;apt-get dist-upgrade
+apt -y -qq install software-properties-common xorg lightdm >>/dev/null 2>&1
+apt-add-repository -y ppa:gnome3 && >>/dev/null 2>&1
+apt-add-repository -y ppa:gnome3-team/gnome3-staging && >>/dev/null 2>&1
+apt-add-repository -y ppa:elementary/stable && >>/dev/null 2>&1
+apt-add-repository -y ppa:elementary/os patches && >>/dev/null 2>&1
+apt update && >>/dev/null 2>&1
+apt-get -y --force-yes -qq install elementary-desktop >>/dev/null 2>&1
+apt-get -y remove unity-greeter && >>/dev/null 2>&1
+dpg-reconfigure pantheon-greeter && >>/dev/null 2>&1
+apt-get upgrade;apt-get dist-upgrade >>/dev/null 2>&1
 }
 plymouthlogo(){
 apt-get install -y plymouth-theme-script > /dev/null #installs script plymouth theme to make changes work.
