dd19b90f15504fccd7a1b4423d1f3187aada354c
Horvan <drubuntu@outlook.com>
2016-05-15 13:29:04 +0200

    fixed elementary os ppas

--- functions.sh
+++ functions.sh
@@ -410,10 +410,10 @@ ln -s /etc/issue.net /etc/issue
 
 elementaryos(){
 apt -y -qq install software-properties-common xorg lightdm >>/dev/null 2>&1
-apt-add-repository -y ppa:gnome3 && >>/dev/null 2>&1
+apt-add-repository -y ppa:gnome3-team/gnome3 && >>/dev/null 2>&1
 apt-add-repository -y ppa:gnome3-team/gnome3-staging && >>/dev/null 2>&1
-apt-add-repository -y ppa:elementary/stable && >>/dev/null 2>&1
-apt-add-repository -y ppa:elementary/os patches && >>/dev/null 2>&1
+apt-add-repository -y ppa:elementary-os/stable && >>/dev/null 2>&1
+apt-add-repository -y ppa:elementary-os/patches && >>/dev/null 2>&1
 apt update && >>/dev/null 2>&1
 apt-get -y --force-yes -qq install elementary-desktop >>/dev/null 2>&1
 apt-get -y remove unity-greeter && >>/dev/null 2>&1
